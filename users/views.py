b74422ccbef5074da580c8bac9f2f2a4b50970ca
Anderson Coelho <anderson.coelho1969@gmail.com>
2023-10-06 19:56:13 -0400

    check the request type and get/verify info on form

--- users/views.py
+++ users/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == "GET":
+        return render(request, 'register.html')
+    elif request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        confirm_password = request.POST.get('confirm_password')
+
+        if not password == confirm_password:
+            return redirect('/users/register')
+        
+        if len(password) < 6:
+            return redirect('/users/register')
+        
+        return HttpResponse("Pass")
